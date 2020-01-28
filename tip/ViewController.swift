8b4ca35c223ff2eb032440d766dc1728d836959f
Christopher Garcia <Chrisgari@hotmail.com>
2020-01-27 17:14:33 -0800

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by user166516 on 1/27/20.
+//  Copyright © 2020 user166516. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
